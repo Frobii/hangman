c53041cd96bc276bd0f031310f46db96c9a80c13
Frobi <frobi.webdev@outlook.com>
2022-10-16 21:04:20 +1030

    implement-guess-checking

--- lib/game.rb
+++ lib/game.rb
@@ -23,8 +23,42 @@ def get_letter(word)
     return word
 end
 
-hangman_word = pick_random_line()
+def lengthen_blank_array(array, hangman_word)
+    i = 0
+    until i == hangman_word.length
+        array.push("_")
+        i += 1
+    end
+    return array
+end
+
+def check_guess(hangman_word, guess_char, correct_guesses)
+    if hangman_word.include?(guess_char)
+        hangman_word.split("").each_with_index { |letter, index| 
+            if letter == guess_char
+                correct_guesses[index] = letter
+            end
+        }
+    end
+end
+
+hangman_word = "apple" #pick_random_line()
 guess_char = get_letter(guess_char)
+correct_guesses = Array.new
+lengthen_blank_array(correct_guesses, hangman_word)
 
 puts hangman_word
 puts guess_char
+p correct_guesses 
+
+check_guess(hangman_word, guess_char, correct_guesses)
+guess_char = get_letter(guess_char)
+p correct_guesses
+check_guess(hangman_word, guess_char, correct_guesses)
+p correct_guesses
+guess_char = get_letter(guess_char)
+check_guess(hangman_word, guess_char, correct_guesses)
+p correct_guesses
+guess_char = get_letter(guess_char)
+check_guess(hangman_word, guess_char, correct_guesses)
+p correct_guesses
